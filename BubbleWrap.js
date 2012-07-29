79becf8edd09b262c15071d6651a15f0dbd3aff5
Joey Casamento <jcsmnt0@gmail.com>
2012-07-28 23:44:51 -0400

    everything works in my very brief testing! documentation and code cleanup soon

--- BubbleWrap.js
+++ BubbleWrap.js
@@ -16,22 +16,19 @@
 // EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 var BubbleWrap = (function() {
-	// based on method from http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
-	var getType = function(thing) {
-		var type = Object.prototype.toString.call(thing);
-		try {
-		   instanceType = Object.prototype.toString.call(new thing());
-		   if (instanceType !== '[object Object]') type = instanceType;
-		} catch (_) {
-		
-		}
-
-		return type.substring(type.indexOf(' ') + 1, type.length - 1);
-	};
+	var builtInTypes =	   [ Object ,  Function ,  Array ,  Number ,  String ,  Boolean ,  RegExp ,  Date ];
+	var builtInTypeNames = ['Object', 'Function', 'Array', 'Number', 'String', 'Boolean', 'RegExp', 'Date'];
 
 	var arrayContains = function(arr, elem) {
 		return arr.indexOf(elem) >= 0;
 	}
+
+	// based on method from http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
+	var getType = function(thing) {
+		if (arrayContains(builtInTypes, thing)) return builtInTypeNames[builtInTypes.indexOf(thing)];
+		var type = Object.prototype.toString.call(thing);
+		return type.substring(type.indexOf(' ') + 1, type.length - 1);
+	};
 
 	var checkValue = function(id, val, validTypes, constraints) {
 		var valType;
@@ -58,14 +55,15 @@ var BubbleWrap = (function() {
 		var constraint, constraintCount, i;
 		if (constraints) {
 			if (getType(constraints) === 'Array') {
+				constraintCount = constraints.length;
 				for (i = 0; i < constraintCount; i++) {
-					if (!constraints[i](val)) {
+					if (!constraints[i](val, id)) {
 						throw 'TypeError: ' + val + ' can\'t be assigned to ' + id + ' because ' +
 							  'it violates a custom constraint.';
 					}
 				}
 			} else {
-				if (!constraints(val)) {
+				if (!constraints(val, id)) {
 					throw 'TypeError: ' + val + ' can\'t be assigned to ' + id + ' because ' +
 						  'it violates a custom constraint.';
 				}
@@ -92,55 +90,59 @@ var BubbleWrap = (function() {
 				definition = obj[id];
 				var defType = getType(definition);
 
-				try {
-					if (definition === new definition().constructor) {
-						if (defType === 'Function' && definition !== Function) {
-							// function value (constant by default)
-							value = definition;
-							setter = (function(obj, id, definition) {
-								return function(val) {
-									checkValue(id, val, null, definition);
-									obj['_' + id] = val;
-								}
-							})(obj, id, definition);
-						} else {
-							// built-in type
-							value = undefined;
-							setter = (function(obj, id, type) {
-								return function(val) {
-									checkValue(id, val, type);
-									obj['_' + id] = val;
-								}
-							})(obj, id, getType(definition));
+				if (defType === 'Function' && definition !== Function) {
+					// function value (constant by default)
+					value = definition;
+					setter = (function(obj, id, definition) {
+						return function(val) {
+							checkValue(id, val, null);
+							obj['_' + id] = val;
 						}
-					}
-				} catch (_) {
-					// value or type definition object
-					// (or unsupported type, but I haven't found a way to differentiate those yet)
-					if (getType(definition) === 'Object' && (definition.validTypes || definition.constraints)) {
-						// convert valid types to type strings
-						validTypes = definition.validTypes, validTypeCount = validTypes.length;
-						for (i = 0; i < validTypeCount; i++) {
-							validTypes[i] = getType(validTypes[i]);
-						}
+					})(obj, id, definition);
+				} else if (getType(definition) === 'Object' && (definition.validTypes || definition.constraints)) {
+					// type definition object
 
-						// type definition object
-						value = definition.value;
-						setter = (function(obj, id, validTypes, constraints) {
-							return function(val) {
-								checkValue(id, val, validTypes, constraints);
-								obj['_' + id] = val;
-							}
-						})(obj, id, validTypes, definition.constraints);
+					// convert valid types to type strings
+					validTypes = definition.validTypes;
+					if (getType(validTypes) === 'Array') {
+						validTypeCount = validTypes.length;
+						for (i = 0; i < validTypeCount; i++) {
+							if (arrayContains(builtInTypes, validTypes[i]))
+								validTypes[i] = builtInTypeNames[builtInTypes.indexOf(validTypes[i])];
+							else if (getType(validTypes[i]) !== 'String')
+								throw 'BubbleWrap error: ' + validTypes[i] + ' is neither a ' +
+									  'built-in type nor a type name string.';
+						}
 					} else {
-						value = definition
-						setter = (function(obj, id, type) {
-							return function(val) {
-								checkValue(id, val, type);
-								obj['_' + id] = val;
-							}
-						})(obj, id, getType(definition));
+						if (arrayContains(builtInTypes, validTypes))
+							validTypes = builtInTypeNames[builtInTypes.indexOf(validTypes)];
 					}
+
+					value = definition.value;
+					setter = (function(obj, id, validTypes, constraints) {
+						return function(val) {
+							checkValue(id, val, validTypes, constraints);
+							obj['_' + id] = val;
+						}
+					})(obj, id, validTypes, definition.constraints);
+				} else if (arrayContains(builtInTypeNames, defType)) {
+					// built-in type
+					value = undefined;
+					setter = (function(obj, id, type) {
+						return function(val) {
+							checkValue(id, val, type);
+							obj['_' + id] = val;
+						}
+					})(obj, id, defType);
+				} else {
+					// literal value
+					value = definition
+					setter = (function(obj, id, type) {
+						return function(val) {
+							checkValue(id, val, type);
+							obj['_' + id] = val;
+						}
+					})(obj, id, getType(definition));
 				}
 
 				Object.defineProperty(obj, '_' + id, {
@@ -165,9 +167,22 @@ var BubbleWrap = (function() {
 		return obj;
 	};
 
+	var constraints = wrap({
+		integer: function(val, id) {
+			if (parseFloat(val) == parseInt(val) && !isNaN(val))
+				return true;
+			throw 'TypeError: ' + val + ' can\'t be assigned to integer ' + id + '.';
+		},
+		nonNegative: function(val, id) {
+			if (val < 0)
+				throw 'TypeError: ' + val + ' can\'t be assigned to non-negative ' + id + '.';
+			return true;
+		}
+	})
 
 	return {
 		wrap: wrap,
-		getType: getType
+		getType: getType,
+		constraints: constraints
 	};
 })()
